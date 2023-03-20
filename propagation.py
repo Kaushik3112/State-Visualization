7942742cad8c5b4ed6592b157bfab1cb99cfbdba
Kaushik3112 <kaushik.singirikonda@gmail.com>
2023-03-20 23:41:31 +0530

    Propagator for Hamiltonian evolution written

---
+++ propagation.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numba import njit
+
+
+@njit
+def cossingen(t, w_list, j, n):
+    temp_var = 1
+
+    if j > 1:
+        for i in range(j-1):
+            temp_var = 2*temp_var*np.cos(w_list[i]*t)
+    elif j == n:
+        temp_var = 2*temp_var*np.cos(w_list[j]*t)
+    else:
+        temp_var = 2*temp_var*np.sin(w_list[j]*t)
+
+    return temp_var
+
+
+@njit
+def v_gen(t, w_list, v_list, n):
+    temp_var = 0
+
+    for i in range(n):
+        temp_var = temp_var + w_list[i+1]*cossingen(t, w_list, i, n)
+
+    return temp_var
+
+
+@njit
+def Ham_gen(t, n, w_0, w_list, v_list, OmegaX, OmegaY, OmegaZ):
+    return (w_0*OmegaZ + w_list[0]*OmegaY + v_gen(t, w_list, v_list, n)*OmegaY)
+
+
+@njit
+def propagation(v_list, c_list, w_list, M0, technique):
+
+    if technique == "Unitary":
+        A = 0     # place holder
+
+    elif technique == "ODE44":
+        A = 1
+
+    return A
